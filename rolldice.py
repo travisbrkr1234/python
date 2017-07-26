64a33e30930b2fc9af63439e0cdc46175a383fdb
Carlos <travisbrkr1234@users.noreply.github.com>
2017-07-26 00:15:52 -0700

    initial python examples and get-pip for windows

---
+++ rolldice.py
@@ -0,0 +1,12 @@
+import random
+
+# This is a simple dice rolling game that exits on any other input that is not y
+while True:
+    userIn = input("Would you like to roll the dice?")
+
+    if userIn == "y":
+        print(random.randint(1,12))
+
+    else:
+        break
+        print("thanks for playing!")
